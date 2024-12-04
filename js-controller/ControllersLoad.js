5c5e3a86d7121be47dd5fd4cc0573275f4c57f35
UvinduV <uvindusanketh470@gmail.com>
2024-12-05 00:33:11 +0530

    load staff names

--- js-controller/ControllersLoad.js
+++ js-controller/ControllersLoad.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
     fetchFieldNames("field_details");
     fetchFieldNames("staff_field_details");
     fetchFieldNames("log_field_details");
-    /*fetchStaffNames("log_staff_details");
-    fetchStaffNames("vehicle_staff_details");*/
+    fetchStaffNames("log_staff_details");
+    fetchStaffNames("vehicle_staff_details");
     fetchFieldNames("equip_field_details");
-    /*fetchStaffNames("equip_staff_details");*/
+    fetchStaffNames("equip_staff_details");
 });
 
 function fetchFieldNames(targetElementId) {
@@ -29,6 +29,30 @@ function fetchFieldNames(targetElementId) {
         },
         error: function (xhr, status, error) {
             console.error("Error loading field names:", status, error);
+        },
+    });
+}
+function fetchStaffNames(targetElementId) {
+    $.ajax({
+        url: " http://localhost:5050/greenShadowCrop/api/v1/staff/getAllStaffNames",
+        type: "GET",
+        contentType: "application/json",
+        success: function (response) {
+            console.log("Staff name: ", response);
+
+            $(`#${targetElementId}`)
+                .empty()
+                .append($("<option>", { value: "", text: "Select A Member" }));
+
+            response.forEach((staff) => {
+                console.log(staff);
+                $(`#${targetElementId}`).append(
+                    $("<option>", { value: staff, text: staff })
+                );
+            });
+        },
+        error: function (xhr, status, error) {
+            console.error("Error loading staff member names:", status, error);
         },
     });
 }
