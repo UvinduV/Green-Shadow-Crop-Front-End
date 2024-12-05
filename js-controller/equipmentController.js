a1a2c6e132b552ed7d0d81dc94275fa670259889
UvinduV <uvindusanketh470@gmail.com>
2024-12-06 00:39:03 +0530

    create save equipment

--- js-controller/equipmentController.js
+++ js-controller/equipmentController.js
@@ -57,3 +57,79 @@ function loadEquipment() {
         },
     });
 }
+function saveEquipment() {
+    var equipment_name = $("#equipment_name").val();
+    var equipment_type = $("#equipment_type").val();
+    var equipment_status = $("#equipment_status").val();
+    var assigned_staff = $("#equip_staff_details").val();
+    var assigned_field = $("#equip_field_details").val();
+
+    $.ajax({
+        url: `http://localhost:5050/greenShadowCrop/api/v1/staff/getStaffId/${assigned_staff}`,
+        type: "GET",
+        success: function (staffId) {
+            console.log("load Staff Id:", staffId);
+            $.ajax({
+                url: `http://localhost:5050/greenShadowCrop/api/v1/fields/getFieldCode/${assigned_field}`,
+                type: "GET",
+                success: function (fieldCode) {
+                    console.log("load field code:", fieldCode);
+                    $.ajax({
+                        url: "http://localhost:5050/greenShadowCrop/api/v1/equipment",
+                        type: "POST",
+                        contentType: "application/json",
+                        data: JSON.stringify({
+                            name: equipment_name,
+                            type: equipment_type,
+                            status: equipment_status,
+                            assignedStaff: {
+                                staffId: staffId
+                            },
+                            assignedField: {
+                                fieldCode: fieldCode
+                            },
+                        }),
+                        success: function (result) {
+                            clearEquipForm();
+                            loadEquipment();
+                            console.log(result);
+                            Swal.fire({
+                                title: "Equipment Save",
+                                text: "Equipment Successfully Saved",
+                                icon: "success"
+                            });
+                        },
+                        error: function (result) {
+                            clearEquipForm();
+                            Swal.fire({
+                                title: "Equipment Save",
+                                text: "Equipment Save Unsuccessfull",
+                                icon: "error"
+                            });
+                            console.log(result);
+                        },
+                    });
+
+                },
+                error: function (error) {
+                    alert("Error loading field code: " + error.responseText);
+                    console.error(error);
+                },
+            });
+
+        },
+        error: function (error) {
+            alert("Error loading staff id: " + error.responseText);
+            console.error(error);
+        },
+    });
+}
+
+function clearEquipForm() {
+    $("#equip_id").val("");
+    $("#equipment_name").val("");
+    $("#equipment_type").val("");
+    $("#equipment_status").val("");
+    $("#equip_staff_details").val("");
+    $("#equip_field_details").val("");
+}
